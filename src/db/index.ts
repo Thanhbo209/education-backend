4a201aea2d7ae714b120c03009cbbf30ca49f3db
Thanh Pham <thanhagar123@gmail.com>
2026-01-15 18:09:50 +0700

    implement database schema

---
+++ src/db/index.ts
@@ -0,0 +1,10 @@
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/neon-http"; // Change this import based on the selected driver
+import { neon } from "@neondatabase/serverless"; // Change this import based on the selected driver
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not defined");
+}
+
+const sql = neon(process.env.DATABASE_URL); // Change this line based on the selected driver
+export const db = drizzle(sql); // Change this line based on the selected driver
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/node-postgres";
 
 const app = express();
 const port = 3000;
 app.use(express.json());
+const db = drizzle(process.env.DATABASE_URL!);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
