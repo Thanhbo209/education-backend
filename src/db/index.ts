86e3e24f75f21b8ef67f32d29ec1434ca455afe7
Thanh Pham <thanhagar123@gmail.com>
2026-01-20 20:15:21 +0700

    fix env issue

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,8 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/neon-http"; // Change this import based on the selected driver
 import { neon } from "@neondatabase/serverless"; // Change this import based on the selected driver
+import dotenv from "dotenv";
+dotenv.config();
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not defined");
